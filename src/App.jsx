e474ab5f5a49238b9b71a67fbf7cc43b519127ad
Nada <NadaMohamedGaber20@gmail.com>
2025-06-11 13:57:04 +0300

    5commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,18 @@ import FirstPage from './FirstPage/FirstPage'
 import About from './About/About'
 import Portfolio from './Portfolio/Portfolio'
 import Contact from './Contact/Contact'
-import { createBrowserRouter , RouterProvider } from 'react-router-dom'
-
-const basename = import.meta.env.PROD ? '/app1-project' : '/';
+import { createBrowserRouter , RouterProvider , createHashRouter } from 'react-router-dom'
 
 function App() {
-  let route = createBrowserRouter([
-    {path: '/',element: <FirstPage />,children:[ 
+  let route = createHashRouter([
+    {path: '',element: <FirstPage />,children:[ 
         { path: '', element: <Home /> },
         { path: 'About', element: <About /> },
         { path: 'Portfolio', element: <Portfolio /> },
         { path: 'Contact', element: <Contact /> }
       ]
     }
-  ],
-  basename
-)
+  ])
   return (
     <>
       <RouterProvider router={route} />
